b97de9bac6b10f7039b75af9efed081d7d9c5279
Elena Nikiforova <vizualnoe@gmail.com>
2022-07-28 14:31:35 +0300

    added Intro layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Intro } from "../Intro/Intro";
 
 function App() {
   return (
     <div className="App">
       <p>Prozhito</p>
+      <Intro />
     </div>
   );
 }
