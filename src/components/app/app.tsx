0d0b11ee898eecacef68f1ae9180c4b4d02d9b88
Elena Nikiforova <vizualnoe@gmail.com>
2022-07-29 00:33:01 +0300

    added IntroItem file

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,13 @@
 import React, {FC} from 'react';
 import styles from './app.module.css';
 import NewsPage from "../../pages/news-page/news-page";
+import { Intro } from '../Intro/Intro';
 
 const App: FC = () => {
   return (
     <div className={styles.app}>
       <p>Prozhito</p>
+      <Intro />
       <NewsPage />
     </div>
   );
