102409c061c51ad5cbcc5500675ed7105bf0bcb1
Alexander Kamordin <akamordin@yandex-team.ru>
2022-07-28 05:44:23 +0300

    Подготовка данных (начало)

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './services/store'
+import {createRoot} from "react-dom/client";
 
+function AppRender() {
+  return (
+    <React.StrictMode>
+      <Provider store={configureStore}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  )
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={configureStore}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = createRoot(rootElement);
+root.render(<AppRender />);
 
