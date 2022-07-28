a8014a0b9e48de4c8580a8fe29b983c49750754f
Elena Nikiforova <vizualnoe@gmail.com>
2022-07-28 14:31:35 +0300

    added Intro layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import styles from './app.module.css';
 import NewsPage from "../../pages/news-page/news-page";
-git 
+
 const App: FC = () => {
   return (
     <div className={styles.app}>
