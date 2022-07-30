966898a2781a0f09259e1e28cb1702dcb6954707
Elena Nikiforova <vizualnoe@gmail.com>
2022-07-30 19:25:54 +0300

    added LinkButton

--- src/components/intro-item/intro-item.tsx
+++ src/components/intro-item/intro-item.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import introItemStyles from "./intro-item.module.css";
+import { LinkButton } from "../link-button/link-button";
 
 export interface IntroItemProps {
   title: string;
@@ -20,13 +21,7 @@ export const IntroItem: FC<IntroItemProps> = (introData: IntroItemProps) => {
       </p>
       <div className={introItemStyles.intro__linkContainer}>
         <p className={introItemStyles.intro__aboutLink}>{action}</p>
-        <a href={link} className="button button_size-intro">
-          <img
-            src="images/Arrow_right_intro.svg"
-            alt="Стрелка влево"
-            className="arrow arrow_intro"
-          />
-        </a>
+        <LinkButton to={link} type="round"/>
       </div>
     </li>
   )
