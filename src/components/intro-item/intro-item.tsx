dc0edaae9f5b4501c9934ee80551204380ff7fa1
Elena Nikiforova <vizualnoe@gmail.com>
2022-07-30 13:15:09 +0300

    amended props

--- src/components/intro-item/intro-item.tsx
+++ src/components/intro-item/intro-item.tsx
@@ -8,17 +8,19 @@ export interface IntroItemProps {
   link: string;
 }
 
-export const IntroItem: FC<IntroItemProps> = (props: IntroItemProps) => {
+export const IntroItem: FC<IntroItemProps> = (introData: IntroItemProps) => {
+
+ const { title, text, action, link } = introData;
 
   return(
     <li className={introItemStyles.intro__card}>
-      <h3 className={introItemStyles.intro__cardTitle}>{props.title}</h3>
+      <h3 className={introItemStyles.intro__cardTitle}>{title}</h3>
       <p className={introItemStyles.intro__cardSubtitle}>
-      {props.text}
+      {text}
       </p>
       <div className={introItemStyles.intro__linkContainer}>
-        <p className={introItemStyles.intro__aboutLink}>{props.action}</p>
-        <a href={props.link} className="button button_size-intro">
+        <p className={introItemStyles.intro__aboutLink}>{action}</p>
+        <a href={link} className="button button_size-intro">
           <img
             src="images/Arrow_right_intro.svg"
             alt="Стрелка влево"
