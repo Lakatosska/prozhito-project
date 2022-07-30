dff4d8155439d448038895340eb94cb7399f9e50
Elena Nikiforova <vizualnoe@gmail.com>
2022-07-30 14:43:02 +0300

    added MaterialsItem component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,12 +4,14 @@ import NewsPage from "../../pages/news-page/news-page";
 import MainPage from "../../pages/main-page/main-page";
 import JournalPage from "../../pages/journal-page/journal-page";
 import { Intro } from '../intro/intro';
+import { Materials } from '../materials/materials';
 
 const App: FC = () => {
   return (
     <div className={styles.app}>
       <p>Prozhito</p>
       <Intro />
+      <Materials />
       <MainPage />
       <NewsPage />
       <JournalPage />
--- src/components/materials/materials.tsx
+++ src/components/materials/materials.tsx
@@ -1,1 +1,37 @@
-export {}
+import { FC } from "react";
+import { dataAPI } from "../../services/api/data";
+import { IDiaryItem } from "../../services/types/diary";
+import materialsStyle from "./materials.module.css";
+
+export const Materials: FC = () => {
+
+  const {isLoading: isDiaryLoading, data: diaryData} = dataAPI.useGetDiariesQuery();
+
+  return(
+    <>
+      <h2 className={materialsStyle.title}>
+        Материалы
+      </h2>
+
+      <ul className="cards">
+      </ul>
+
+      {
+        !isDiaryLoading && diaryData &&
+        <ul style={{display: 'flex', flexDirection: 'row', listStyleType: 'none'}}>
+          {
+            diaryData.map((item: IDiaryItem) => (
+              <li key={item.id}>
+                <div style={{padding: '10px'}}>
+                  <p>{item.name}</p>
+                  <img width={200} height={224} src={require(`../../images/${item.image}`)} alt={'Картинка дневник'}/>
+                  <p>{item.text}</p>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      }
+    </>
+  )
+}
