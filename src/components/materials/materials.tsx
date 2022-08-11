02bca388793da6e6a683d183acd70fbd90b8148f
Elena Soloveva <seaver@yandex.ru>
2022-08-11 15:26:06 +0300

    feat: add slider to materials mobile

--- src/components/materials/materials.tsx
+++ src/components/materials/materials.tsx
@@ -2,19 +2,32 @@ import { FC } from "react";
 import { IDiaryItem } from "../../services/types/diary";
 import materialsStyle from "./materials.module.css";
 import { MaterialsItem } from "../materials-item/materials-item";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import useMediaQuery from "../../hooks/useMediaQuery";
+import styles from "../cards-slider/cards-slider.module.css";
 
 interface IMaterialsProps{
   data: IDiaryItem[]
 }
 export const Materials: FC<IMaterialsProps> = ({data}) => {
+  const desktop = useMediaQuery("(min-width: 768px)");
 
-console.log(data)
+
+  console.log(data)
   return(
     <section className={materialsStyle.materials}>
       <h2 className={materialsStyle.title}>
         Материалы
       </h2>
-        <ul className={materialsStyle.cards}>
+      {desktop
+      ? (<ul className={materialsStyle.cards}>
           {data.map((item) =>
             <MaterialsItem
               id={item.id}
@@ -26,7 +39,35 @@ console.log(data)
               sample={item.sample}
             />
           )}
-        </ul>
+        </ul>)
+      : (
+        <>
+        <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={12}
+        slidesPerView={1.1}
+        pagination={{ el: '.pagination',
+          clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        className={materialsStyle.swiper}
+        >
+          {data.map((item) =>
+            <SwiperSlide><MaterialsItem
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              text={item.text}
+              tag={item.tag}
+              key={item.id}
+              sample={item.sample}
+            /></SwiperSlide>
+          )}
+        </Swiper>
+          <div className={`${materialsStyle.pagination} pagination`}/>
+        </>
+        )}
     </section>
   )
 }
