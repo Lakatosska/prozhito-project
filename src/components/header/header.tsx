0ef4a0eb9d258819c473d5747865cfaa08448d82
Elena Soloveva <seaver@yandex.ru>
2022-08-11 10:23:13 +0300

    fix: fix header logo links

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent, useState } from "react";
 import headerStyles from "./header.module.css";
-import logo from "../../images/logo.svg";
-import logo_mobile from "../../images/logo_mobile.svg";
+import logo from "../../images/logo_prozhito.svg";
+import logo_mobile from "../../images/logo_prozhito_mobile.svg";
+import logo_eu_ru from '../../images/logo_eu_ru.svg';
 import openImg from "../../images/menu-mobil-open.svg";
 import closeImg from "../../images/menu-mobil-close.svg";
 import HeaderNav from "../headerNav/headerNav";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import footerInfoStyles from "../footer-info/footer-info.module.css";
 
 const Header: FunctionComponent = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -27,7 +29,19 @@ const Header: FunctionComponent = () => {
   return (
     <header className={menuOpen?  `${headerStyles.header} ${headerStyles.header_theme_dark}` : headerStyles.header }>
       <div className={`${headerStyles.header__container} ${headerStyles.menu}`}>
-        <img src={desktop? logo : logo_mobile} alt='Логотип Прожито' />
+        <div className={headerStyles.header__links}><a href="https://prozhito.org/" target="_blank" rel="noopener noreferrer" className={footerInfoStyles.link}>
+          <img
+            src={desktop? logo : logo_mobile}
+            alt={desktop.toString()}
+          />
+        </a>
+        <a href="https://eusp.org/" target="_blank" rel="noopener noreferrer" className={footerInfoStyles.link}>
+          <img
+            src={logo_eu_ru}
+            alt="Европейский университет в Санкт-Петербурге"
+          />
+        </a>
+        </div>
         <HeaderNav desktop={desktop} open={menuOpen}/>
         {!desktop&&<button type='button' className={headerStyles.menu__button} onClick={toggleMobileMenu}>
           <img
