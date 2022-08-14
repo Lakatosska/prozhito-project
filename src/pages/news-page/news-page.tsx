0d7c412abfb3d89daad430292ca2e7819449090d
Elana Tollu <elana.tollu@yandex.ru>
2022-08-14 17:11:08 +0300

    add news card

--- src/pages/news-page/news-page.tsx
+++ src/pages/news-page/news-page.tsx
@@ -1,46 +1,57 @@
 import {FC} from "react";
 import {dataAPI} from "../../services/api/data";
 import {useDispatch, useSelector} from "../../hooks";
-import {dataNewsSelector, pageNewsSelector, totalNewsSelector} from "../../services/selectors/news";
+import {pageNewsSelector} from "../../services/selectors/news";
 import {setNewsPage} from "../../services/slices/news";
 import {NEWS_PAGE_LIMIT} from "../../constants";
-import {formatDate} from "../../utils/dateHelper";
+import NewsCard from "../../components/news-card/news-card";
+import newsPageStyle from './news-page.module.css';
+import { LinkButton } from "../../components/link-button/link-button";
 
 const NewsPage: FC = () => {
   const dispatch = useDispatch();
   const page = useSelector(pageNewsSelector)
-  const total = useSelector(totalNewsSelector)
-  const news = useSelector(dataNewsSelector)
-  const {isLoading: isNewsLoading} = dataAPI.useGetNewsQuery({page, size: NEWS_PAGE_LIMIT});
+  const {isLoading, data} = dataAPI.useGetNewsQuery({page, size: NEWS_PAGE_LIMIT});
   const handleLoad = () => {
     dispatch(setNewsPage(page + 1))
   }
   return (
     <>
-      <h1>Страница новостей</h1>
+      <h1  className={newsPageStyle.title}>Новости и события</h1>
       {
-        !isNewsLoading && news &&
-        <ul style={{listStyleType: 'none'}}>
+        !isLoading && data &&
+        <>
+        <ul className={newsPageStyle.container}>
           {
-            news.map(item => (
+            data.data.map(item => (
               <li key={item.id}>
-                <div style={{padding: '10px'}}>
-                  <p>{item.id}</p>
-                  <p>{item.text}</p>
-                  <img width={253} height={306} src={require(`../../images/${item.image}`)} alt={'Картинка новости'}/>
-                  <p>{formatDate(item.date, "short")}</p>
-                </div>
+                <NewsCard  item={item} />
               </li>
             ))
           }
-          {
-            total > news.length &&
-            <button onClick={handleLoad}>Загрузить еще</button>
-          }
+
         </ul>
+        {
+          data.total > data.data.length &&
+          <div className={newsPageStyle.button}>
+            <LinkButton onClick={handleLoad}>Загрузить еще</LinkButton>
+          </div>
+          }
+        </>
       }
     </>
   )
 }
 
 export default NewsPage
+
+/*
+<li key={item.id}>
+  <div style={{padding: '10px'}}>
+    <p>{item.id}</p>
+    <p>{item.text}</p>
+    <img width={253} height={306} src={require(`../../images/${item.image}`)} alt={'Картинка новости'}/>
+    <p>{formatDate(item.date, "short")}</p>
+  </div>
+</li>
+*/
